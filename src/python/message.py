6820e755de60fe49cb94f1d008b921bcb80e2c44
henning <henna497@student.liu.se>
2016-02-26 12:34:56 +0100

    Add docstring to Message class

--- src/python/message.py
+++ src/python/message.py
@@ -3,6 +3,7 @@ from src.python.generated_ui.message import Ui_message
 
 
 class Message(QtWidgets.QDialog):
+    """ Simple window for showing messages."""
     def __init__(self, message):
         super(Message, self).__init__()
         self.ui = Ui_message()
