b646fdbd3d9a10b1adab99b57b5a277ec6ffeefb
henning <henna497@student.liu.se>
2016-02-26 12:32:07 +0100

    Add error messages in a sepperate widget

---
+++ src/python/message.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWidgets
+from src.python.generated_ui.message import Ui_message
+
+
+class Message(QtWidgets.QDialog):
+    def __init__(self, message):
+        super(Message, self).__init__()
+        self.ui = Ui_message()
+        self.ui.setupUi(self)
+        self.ui.textBrowser.setText(message)
+        self.ui.pushButton.clicked.connect(self.close)
+        self.show()
--- src/python/spmpath.py
+++ src/python/spmpath.py
@@ -1,10 +1,12 @@
 from PyQt5 import QtWidgets
 from src.python.generated_ui.spmpathph import Ui_spm_path
-from src.python.sessionmanager import SessionManager
+from src.python.message import Message
 import os.path
 
 
 class SPMPath(QtWidgets.QDialog):
+    """Window for adding a path to SPM directory
+    """
     def __init__(self, manager):
         super(SPMPath, self).__init__()
         self.manager = manager
@@ -13,18 +15,32 @@ class SPMPath(QtWidgets.QDialog):
         self.ui.submit.clicked.connect(self.button_pressed)
         self.show()
 
-    def button_pressed(self,e):
-        # retrieves and prints what is in the variable 'data' in matlab
-        path = self.ui.lineEdit.text()
-        if os.path.exists(os.path.expanduser(path)):
+    def button_pressed(self, e):
+        """Chech and save given SPM path
+        :param e: Event description
+        """
+        if self.check_spm():
+            Message("SPM path already set").exec_()
+            self.close()
+
+        path = os.path.expanduser(self.ui.lineEdit.text())
+        if self.check_path(path):
             self.manager.session.addpath(path, nargout=0)
-            status = self.manager.session.exist('spm', nargout=1)
-            if status != 2: #
-                self.ui.submit.setText("SPM is not working")
-                self.manager.session.rmpath(path, nargout=0)
-            else:
-                self.ui.submit.setText("Working")
-                self.close()
+            if self.check_spm():
+                Message("Path has been added").exec_()
                 # TODO save path to settings
+                self.close()
+            else:
+                Message("SPM is not working").exec_()
+                self.manager.session.rmpath(path, nargout=0)
         else:
-            self.ui.submit.setText("Not a valid directory")
+            Message("Not an existing directory").exec_()
+
+
+    def check_path(self, path):
+        """Check if given path is a valid SPM directory."""
+        return os.path.exists(path)
+
+    def check_spm(self):
+        """Check if spm function exists in matlab engine."""
+        return self.manager.session.exist('spm', nargout=1) == 2
