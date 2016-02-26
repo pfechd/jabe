fe41f04e93bdecbb9bfc0ed14bd80a717d1e25dc
David Bergström <david-bergstrom@outlook.com>
2016-02-26 14:07:31 +0100

    Ensure valid variable names for load stimuli

--- src/matlab/load_stimuli.m
+++ src/matlab/load_stimuli.m
@@ -5,6 +5,7 @@ function y = load_stimuli(path, tr)
     v1 = [visual_stimuli(:,1)/tr];
     v1 = floor(v1);
     v1 = [v1 visual_stimuli(:,2)];
-    set_data(path,v1);
-    y = path;
+    variable_name = matlab.lang.makeValidName(path);
+    set_data(variable_name,v1);
+    y = variable_name;
 end
