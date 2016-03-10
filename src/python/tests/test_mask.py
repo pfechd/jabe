0f4ce26baa28091599b7c1038b795d00e0c4458b
Calle Arvidsson <calle.arvidssons@gmail.com>
2016-03-10 21:50:21 +0100

    Add stimuli test data

---
+++ src/python/tests/test_mask.py
@@ -0,0 +1,22 @@
+import unittest
+import matlab.engine
+from src.python import mask
+
+class TestBrain(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.session = matlab.engine.connect_matlab()
+        cls.mask = mask.Mask('../python/tests/test-data/mask1.nii', cls.session)
+        cls.session.load_test_data(nargout=0)
+
+    def test_mask_loaded_correctly(self):
+        ref = self.session.get_data('mask_ref')
+        loaded_mask = self.session.get_data(self.mask.id)
+        self.assertEqual(self.session.isequal(ref, loaded_mask), True)
+
+    def tearDown(self):
+        self.session = None
+        self.mask = None
+
+if __name__ == '__main__':
+    unittest.main()
