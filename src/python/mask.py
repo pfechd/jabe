7d909b1c9e02ec4676a2c1ba35b7f2d20084c4d1
Filip Magnusson <filma035@student.liu.se>
2016-04-05 11:25:00 +0200

    Add possibility of making a sphere mask

--- src/python/mask.py
+++ src/python/mask.py
@@ -9,6 +9,7 @@ class Mask:
     accessed through the member called data.
     """
     def __init__(self, path = None, coordinates = None, size = None, shape = None, shape_size = None):
+        # if we get a path then we load that path, otherwise we make a new mask with the specified data
         if path is not None:
             self.path = path
             mask_file = nib.load(path)
@@ -18,10 +19,18 @@ class Mask:
             self.data = np.zeros(size)
 
                 
-            # set ones on the correct coordinate and with a specified shape
+            # set ones in a volume of a cube around the specified coordinate
             if shape == "cube":
-                for z in range(coordinates[2] - shape_size/2, coordinates[2] + shape_size/2):
-                    for y in range(coordinates[1] - shape_size/2, coordinates[1] + shape_size/2):
-                        for x in range(coordinates[0] - shape_size/2, coordinates[0] + shape_size/2):
+                for z in range(coordinates[2] - shape_size/2, coordinates[2] + shape_size/2+1):
+                    for y in range(coordinates[1] - shape_size/2, coordinates[1] + shape_size/2+1):
+                        for x in range(coordinates[0] - shape_size/2, coordinates[0] + shape_size/2+1):
                             self.data[z,y,x] = 1
+
+            # set ones in every coordinate within the distance shape_siez around he coordinate, making it a sphere
+            if shape == "sphere":
+                for z in range(0,size[2]):
+                    for y in range(0,size[1]):
+                        for x in range(0,size[0]):
+                            if (coordinates[2]-z)**2 + (coordinates[1] - y)**2 + (coordinates[0] - x)**2 <= shape_size**2:
+                                self.data[z,y,x] = 1
 
