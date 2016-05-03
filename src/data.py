de7a131474cbe6a1799be7c035e1de88d77d8cdf
David Bergström <david-bergstrom@outlook.com>
2016-05-03 14:25:50 +0200

    Move remaining methods from Group to Data

--- src/data.py
+++ src/data.py
@@ -6,7 +6,9 @@ from src.stimulionset import StimuliOnset
 
 
 class Data(object):
-    def __init__(self):
+    def __init__(self, name=None, configuration=None):
+        self.name = name
+        self.sessions = []
         self.name = None
 
         # The path to the file containing brain information
@@ -31,6 +33,11 @@ class Data(object):
         # Result of calculations are kept here
         self.masked_data = None
         self.responses = {}
+
+        if configuration:
+            self.load_configuration(configuration)
+        elif name:
+            pass
 
     def ready_for_calculation(self, stimuli=None, mask=None):
         if not stimuli:
@@ -236,3 +243,63 @@ class Data(object):
             responses_sem[stimuli_type] = response_sem
 
         return responses_sem
+
+    def get_configuration(self):
+        return {
+            'name': self.name,
+            'individuals': [individual.get_configuration() for individual in self.children],
+            'sessions': [session.get_configuration() for session in self.sessions]
+        }
+
+    def load_configuration(self, configuration):
+        if 'name' in configuration:
+            self.name = configuration['name']
+
+    def add_session(self, session):
+        self.sessions.append(session)
+
+    def remove_session(self, session):
+        self.sessions.remove(session)
+
+    def combine_children_responses(self):
+        self.responses = {}
+        children = self.children + self.sessions
+
+        for child in children:
+            # If the child doesn't have the files loaded, skip it.
+            if not child.ready_for_calculation():
+                continue
+            session_response = child.calculate_mean()
+            for intensity, data in session_response.iteritems():
+                # Convert data into 1xN matrix to be able to properly concatenate into the result
+                data = data.reshape(1, data.shape[0])
+                if intensity in self.responses:
+                    self.responses[intensity] = np.concatenate((self.responses[intensity], data))
+                else:
+                    self.responses[intensity] = data
+
+    def _aggregate(self, percentage, global_, mask, stimuli):
+        self.responses = {}
+        min_width = float('inf')
+
+        for child in self.children + self.sessions:
+            # If the child doesn't have the files loaded, skip it.
+            if not child.ready_for_calculation():
+                continue
+            child_response = child.aggregate(percentage, global_, mask, stimuli)
+
+            for intensity, data in child_response.iteritems():
+                min_width = min(min_width, data.shape[1])
+
+                if intensity in self.responses:
+                    responses = self.responses[intensity][:, 0:min_width]
+                    data = data[:, 0:min_width]
+                    self.responses[intensity] = np.concatenate((responses, data))
+                else:
+                    self.responses[intensity] = data
+
+        # Set all data to match the length of the least wide response
+        for intensity, data in self.responses.iteritems():
+            self.responses[intensity] = data[:, 0:min_width]
+
+        return self.responses
