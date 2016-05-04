72b347e2a15704b8de6a6be12b7c8ef17ec5d278
Håkan Gudmundsson <hakgu806@student.liu.se>
2016-05-04 16:05:50 +0200

    All functions implemented, ready for merge

--- src/stimuliwindow.py
+++ src/stimuliwindow.py
@@ -9,7 +9,7 @@ from src.generated_ui.create_stimuli import Ui_Stimuli_window
 
 class StimuliWindow(QDialog):
     """
-    Window for creating a stimuli by manually putting in values in a table.
+    Window for creating a stimuli by manually putting in onset times and stimule values in a table.
     """
     
     def __init__(self, parent):
@@ -24,6 +24,7 @@ class StimuliWindow(QDialog):
         self.ui.setupUi(self)
         self.ui.cancel.clicked.connect(self.close_window)
         self.ui.add_row.clicked.connect(self.add_row)
+        self.ui.remove_row.clicked.connect(self.remove_row)
         self.ui.create_stimuli.clicked.connect(self.save_stimuli)
         self.show()
 
@@ -31,6 +32,10 @@ class StimuliWindow(QDialog):
         rows = self.ui.stimuli_table.rowCount()
         self.ui.stimuli_table.setRowCount(rows+1)
 
+    def remove_row(self):
+        rows = self.ui.stimuli_table.rowCount()
+        self.ui.stimuli_table.setRowCount(rows-1)
+        
     def close_window(self):
         self.close()
 
@@ -39,6 +44,7 @@ class StimuliWindow(QDialog):
         Tries to create an array of the values in the table. 
 
         :return: If successful, an array representing the table. If not, an empty array.
+        :return example: array = [[12.3, 60], [13.7, 90], [15.2, 40]]
         """
         
         stimuli = []
@@ -50,7 +56,6 @@ class StimuliWindow(QDialog):
             for row in xrange(0, all_rows):
                 # Goes through all columns in all rows of the table, converts them to
                 # float and adds them to the stimuli array if they are numbers
-                onset = []
                 time = self.ui.stimuli_table.item(row, 0).text()
                 value = self.ui.stimuli_table.item(row,1).text()
                 if self.is_number(time) and self.is_number(value):
@@ -62,17 +67,17 @@ class StimuliWindow(QDialog):
         return stimuli
         
     def save_stimuli(self):
-        """ Saves the values from the table to a .mat file"""
+        """ Saves the values from the table to a .mat file."""
 
         stimuli = self.create_stimuli_array()
-            if stimuli:
-                # If the table is valid, eg is not empty, save it and load the created stimuli to the main window
-                filename = QFileDialog.getSaveFileName(self, "Save stimuli", "", ".mat")
-                if filename[0]:
-                    self.close()
-                    stimuli = np.array(stimuli)
-                    sio.savemat(filename[0], {'visual_stimuli':stimuli})
-                    self.parent().load_stimuli(filename[0] + filename[1])
+        if stimuli:
+            # If the table is valid, eg is not empty, save it and load the created stimuli to the main window
+            filename = QFileDialog.getSaveFileName(self, "Save stimuli", "", ".mat")
+            if filename[0]:
+                self.close()
+                stimuli = np.array(stimuli)
+                sio.savemat(filename[0], {'visual_stimuli':stimuli})
+                self.parent().load_stimuli(filename[0] + filename[1])
                 
     def is_number(self, s):
         """
