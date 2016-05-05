2ac4fabff6af440788972cfc69d090d7792f0272
Elin Petersén <elin.petersen@kanal11.se>
2016-05-05 15:31:56 +0200

    Update comments and remove unnecessary code in mask.py

--- src/mask.py
+++ src/mask.py
@@ -25,38 +25,37 @@ class Mask:
             mask_file = nib.load(path)
             self.data = mask_file.get_data()
         else:
-            #self.path = path
-            #brain_file = nib.load(path)
-
             voxel_size = brain_file._header.get_zooms()
-            size = brain_file.get_data().shape[0:2]
+            size = brain_file.get_data().shape[0:3]
 
             # create empty matrix of correct size
             self.data = np.zeros(size)
 
             # Convert coordinates and shape_size to mm
-            radius_width /= voxel_size
-            coordinate = (coordinate[0] / voxel_size, coordinate[1] / voxel_size, coordinate[2] / voxel_size)
+            coordinate = (coordinate[0] / voxel_size[0], coordinate[1] / voxel_size[1], coordinate[2] / voxel_size[2])
 
             # set ones in a volume of a cube around the specified coordinate
             if shape == "cube":
-                for z in range(coordinate[2] - radius_width / 2, coordinate[2] + radius_width / 2 + 1):
-                    for y in range(coordinate[1] - radius_width / 2, coordinate[1] + radius_width / 2 + 1):
-                        for x in range(coordinate[0] - radius_width / 2, coordinate[0] + radius_width / 2 + 1):
+                for z in range(coordinate[2] - radius_width[2] / 2*voxel_size[2],
+                               coordinate[2] + radius_width[2] / 2*voxel_size[2] + 1):
+                    for y in range(coordinate[1] - radius_width[1] / 2*voxel_size[1],
+                                   coordinate[1] + radius_width[1] / 2*voxel_size[1] + 1):
+                        for x in range(coordinate[0] - radius_width[0] / 2*voxel_size[0],
+                                       coordinate[0] + radius_width[0] / 2*voxel_size[0] + 1):
                             self.data[z, y, x] = 1
 
-            # set ones in every coordinate within the distance shape_size around he coordinate, making it a sphere
+            # set ones in every coordinate within the distance radius_width around the coordinate, making it a sphere
             if shape == "sphere":
                 for z in range(0, size[2]):
                     for y in range(0, size[1]):
                         for x in range(0, size[0]):
                             if (coordinate[2] - z) ** 2 + (coordinate[1] - y) ** 2 + (
-                                        coordinate[0] - x) ** 2 <= radius_width ** 2:
+                                        coordinate[0] - x) ** 2 <= radius_width[0] ** 2:
                                 self.data[z, y, x] = 1
 
-
-            mask_file = nib.Nifti1Image(self.data)
-            nib.save(mask_file, os.path.join(path, path.split(os.path.sep)[-1]))
+            # Create a nifti file containing the data and save it to path
+            mask_file = nib.Nifti1Image(self.data, np.eye(4,4))
+            nib.save(mask_file, path)
 
     def get_configuration(self):
         return {'path': self.path}
